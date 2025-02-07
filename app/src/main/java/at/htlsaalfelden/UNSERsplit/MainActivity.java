3bb909c6c4d4cabd790b36e55eb0737a7ed1c4da
lekrotte22 <152809996+lekrotte22@users.noreply.github.com>
2025-02-07 09:22:44 +0100

    ein weiteres init

---
+++ app/src/main/java/at/htlsaalfelden/UNSERsplit/MainActivity.java
@@ -0,0 +1,31 @@
+package at.htlsaalfelden.UNSERsplit;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import at.htlsaalfelden.UNSERsplit.ui.login.LoginActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Intent myIntent = new Intent(this, LoginActivity.class);
+        startActivity(myIntent);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
---
+++ app/src/main/java/at/htlsaalfelden/UNSERsplit/ui/login/LoginActivity.java
@@ -0,0 +1,35 @@
+package at.htlsaalfelden.UNSERsplit.ui.login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import at.htlsaalfelden.UNSERsplit.MainActivity;
+import at.htlsaalfelden.UNSERsplit.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+
+        findViewById(R.id.register).setOnClickListener(v -> {
+            Intent myIntent = new Intent(this, MainActivity.class);
+            startActivity(myIntent);
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
