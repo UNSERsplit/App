48e99f9bef0499b950ad11177d6acc5388d0ab3b
lekrotte22 <152809996+lekrotte22@users.noreply.github.com>
2025-02-25 08:55:46 +0100

    home initialized

--- app/src/main/java/at/htlsaalfelden/UNSERsplit/MainActivity.java
+++ app/src/main/java/at/htlsaalfelden/UNSERsplit/MainActivity.java
@@ -17,12 +17,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
 
         Intent myIntent = new Intent(this, LoginActivity.class);
         startActivity(myIntent);
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
--- app/src/main/java/at/htlsaalfelden/UNSERsplit/ui/login/LoginActivity.java
+++ app/src/main/java/at/htlsaalfelden/UNSERsplit/ui/login/LoginActivity.java
@@ -2,7 +2,6 @@ package at.htlsaalfelden.UNSERsplit.ui.login;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -26,7 +25,7 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(myIntent);
         });
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
