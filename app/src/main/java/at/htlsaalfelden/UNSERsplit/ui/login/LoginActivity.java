4ebca0fcd01e3da596a14b7bd0c6518451ba590b
lekrotte22 <152809996+lekrotte22@users.noreply.github.com>
2025-02-28 08:56:14 +0100

    Settings initialized

--- app/src/main/java/at/htlsaalfelden/UNSERsplit/ui/login/LoginActivity.java
+++ app/src/main/java/at/htlsaalfelden/UNSERsplit/ui/login/LoginActivity.java
@@ -31,10 +31,10 @@ public class LoginActivity extends AppCompatActivity {
             startActivity(myIntent);
         });
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home), (v, insets) -> {
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
+        });*/
     }
 }
--- app/src/main/java/at/htlsaalfelden/UNSERsplit/ui/settings/SettingsActivity.java
+++ app/src/main/java/at/htlsaalfelden/UNSERsplit/ui/settings/SettingsActivity.java
@@ -1,4 +1,4 @@
-package at.htlsaalfelden.UNSERsplit.ui.home;
+package at.htlsaalfelden.UNSERsplit.ui.settings;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -12,29 +12,21 @@ import androidx.core.view.WindowInsetsCompat;
 
 import at.htlsaalfelden.UNSERsplit.MainActivity;
 import at.htlsaalfelden.UNSERsplit.R;
-import at.htlsaalfelden.UNSERsplit.ui.settings.SettingsActivity;
 
-public class HomeActivity extends AppCompatActivity {
-
-
+public class SettingsActivity extends AppCompatActivity {
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_settings);
 
         findViewById(R.id.navHome).setOnClickListener(v -> {
             Intent myIntent = new Intent(this, MainActivity.class);
             startActivity(myIntent);
         });
 
-        findViewById(R.id.navSettings).setOnClickListener(v -> {
-            Intent myIntent = new Intent(this, SettingsActivity.class);
-            startActivity(myIntent);
-        });
-
-        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home), (v, insets) -> {
+       /* ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
