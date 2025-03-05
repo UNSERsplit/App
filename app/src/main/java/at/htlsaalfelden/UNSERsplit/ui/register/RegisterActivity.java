5d89413363d235de8cdf8346d158d087e3b0e36a
lekrotte22 <152809996+lekrotte22@users.noreply.github.com>
2025-03-05 12:07:04 +0100

    Refactored LoginXML - made responsive

--- app/src/main/java/at/htlsaalfelden/UNSERsplit/ui/register/RegisterActivity.java
+++ app/src/main/java/at/htlsaalfelden/UNSERsplit/ui/register/RegisterActivity.java
@@ -1,11 +1,14 @@
 package at.htlsaalfelden.UNSERsplit.ui.register;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import at.htlsaalfelden.UNSERsplit.MainActivity;
 import at.htlsaalfelden.UNSERsplit.R;
+import at.htlsaalfelden.UNSERsplit.ui.login.LoginActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -15,7 +18,10 @@ public class RegisterActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_register);
 
-
+        findViewById(R.id.btnRegistrieren).setOnClickListener(v -> {
+            Intent myIntent = new Intent(this, LoginActivity.class);
+            startActivity(myIntent);
+        });
 
         /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.home), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
